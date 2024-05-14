ccb9c4a115f3b82babd722219dff3d3dbb4725ec
Patrick Baxter <patrickb321@gmail.com>
2024-05-15 10:47:41 +1200

    android: update version 3.5.0

--- sidekick/app/build.gradle.kts
+++ sidekick/app/build.gradle.kts
@@ -15,8 +15,8 @@ if (performSigning) {
 val sdk = 34
 val minSdkVersion = 22
 val majorVersion = 3
-val minorVersion = 4
-val patchVersion = 1
+val minorVersion = 5
+val patchVersion = 0
 android {
     buildToolsVersion = "34.0.0"
     namespace = "nz.org.cacophony.sidekick"
