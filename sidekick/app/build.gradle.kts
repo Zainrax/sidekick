1254ffa4e31644dbe7cbe0450a2cd51a3d2541b8
Patrick Baxter <patrickb321@gmail.com>
2025-02-18 10:57:18 +1300

    update minor 3.10.0

--- sidekick/app/build.gradle.kts
+++ sidekick/app/build.gradle.kts
@@ -15,8 +15,8 @@ if (performSigning) {
 val sdk = 34
 val minSdkVersion = 22
 val majorVersion = 3
-val minorVersion = 9
-val patchVersion = 1
+val minorVersion = 10
+val patchVersion = 0
 android {
     buildToolsVersion = "34.0.0"
     namespace = "nz.org.cacophony.sidekick"
